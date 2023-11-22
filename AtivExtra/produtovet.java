935848984beb386829535980f5d752de59b0566c
Renan Camara <151562116+renancamarafarias@users.noreply.github.com>
2023-11-22 01:38:44 -0300

    produtoescalar.java

--- AtivExtra/produtovet.java
+++ AtivExtra/produtovet.java
@@ -1,40 +1,33 @@
 import javax.swing.JOptionPane;
 
 public class produtovet {
+
         public static void main(String[] args) {
         // Input
         String tamS = JOptionPane.showInputDialog("Insira o tamanho dos vetores: ");
         int tam = Integer.parseInt(tamS);
 
         // Definir arrays
-        int[] vetor1 = new int[tam];
-        int[] vetor2 = new int[tam];
-        int[] produto = new int[tam];
-        String resposta = "";
+        float[] vetor1 = new float[tam];
+        float[] vetor2 = new float[tam];
+        float produto = 0;
+
         // Input dos vetores
         for (int i = 0; i < vetor1.length; i++) {
             String valorS = JOptionPane.showInputDialog(null, "Elemento "+ i + ": ", "Insira os elementos do primeiro vetor", 1);
-            vetor1[i] = Integer.parseInt(valorS);
+            vetor1[i] = Float.parseFloat(valorS);
         }
         for (int i = 0; i < vetor2.length; i++) {
             String valorS = JOptionPane.showInputDialog(null, "Elemento "+ i + ": ", "Insira os elementos do segundo vetor", 1);
-            vetor2[i] = Integer.parseInt(valorS);
+            vetor2[i] = Float.parseFloat(valorS);
         }
 
         // Multiplicar elementos
         for (int i = 0; i < vetor2.length; i++) {
-            produto[i] = vetor1[i] * vetor2[i];
+            produto += vetor1[i] * vetor2[i];
         }
 
-        // Print array
-        for (int i = 0; i < produto.length; i++) {
-            if (i == produto.length-1) {
-                resposta = resposta + produto[i];
-            }
-            else{
-            resposta = resposta + produto[i]+", " ;
-            }
-        }
-        JOptionPane.showMessageDialog(null, "["+resposta+"]", "A produto dos vetores", 1);
+        JOptionPane.showMessageDialog(null, "O produto escalar dos vetores é: "+ produto);
+  
     }
 }
